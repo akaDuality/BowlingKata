c03a7363f2fd99b07a8074662be5d4013dd8f30d
Mikhail Rubanov <akaDuality@yandex.ru>
2019-09-02 17:07:28 +0500

    Rework roll method to frames

--- Bowling/Game.swift
+++ Bowling/Game.swift
@@ -12,7 +12,7 @@ class Game {
     
     private var rolls: [Int] = []
     
-    func roll(_ pins: Int) {
+    func roll2(_ pins: Int) {
         score += pins
         
         rolls.append(pins)
@@ -22,6 +22,29 @@ class Game {
             if isFirstRollInFrame {
                 let isSpare = rolls[i-1] + rolls[i-2] == 10
                 if isSpare {
+                    score += pins
+                }
+            }
+        }
+    }
+    
+    private var frames: [Frame] = []
+    
+    func roll(_ pins: Int) {
+        if var lastFrame = frames.last, !lastFrame.isFinished {
+            lastFrame.roll2 = pins
+            frames[frames.count - 1] = lastFrame
+        } else {
+            let frame = Frame(roll1: pins)
+            frames.append(frame)
+        }
+        
+        score += pins
+        if frames.count > 1 {
+            let isFirstRollInFrame = frames.last!.roll2 == nil
+            if isFirstRollInFrame {
+                let previousFrame = frames[frames.count - 2]
+                if previousFrame.isSpare {
                     score += pins
                 }
             }
