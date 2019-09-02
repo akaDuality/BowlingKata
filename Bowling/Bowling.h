99bfecd20b09ef03106e9d114ec46ab542052141
Mikhail Rubanov <akaDuality@yandex.ru>
2019-09-02 14:45:13 +0500

    Initial Commit

---
+++ Bowling/Bowling.h
@@ -0,0 +1,19 @@
+//
+//  Bowling.h
+//  Bowling
+//
+//  Created by Mikhail Rubanov on 02/09/2019.
+//  Copyright © 2019 akaDuality. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bowling.
+FOUNDATION_EXPORT double BowlingVersionNumber;
+
+//! Project version string for Bowling.
+FOUNDATION_EXPORT const unsigned char BowlingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bowling/PublicHeader.h>
+
+
