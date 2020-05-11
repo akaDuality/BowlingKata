43102947ab515949b7ee44cbc34e07c45665ddec
Mikhail Rubanov <m.rubanov@dodopizza.com>
2020-05-11 09:32:07 +0300

    Some

--- BowlingTests/GameTests.swift
+++ BowlingTests/GameTests.swift
@@ -41,130 +41,125 @@ class GameSpec: QuickSpec {
             }
             
             // MARK: - Spare
-            describe("spare") {
-                context("when roll spare") {
+            context("when roll spare") {
+                beforeEach {
+                    game.spare(first: 4)
+                }
+                
+                it("should double next roll") {
+                    game.roll(6)
+                    expect(game.score) == 4 + 6 + 6 * 2
+                }
+                
+                context("when next 2 roll also spare") {
                     beforeEach {
-                        game.roll(2)
-                        game.roll(8)
+                        game.spare(first: 6)
                     }
                     
-                    it("should double next roll") {
-                        game.roll(4)
-                        expect(game.score) == 2 + 8 + 4 * 2
+                    it("should double 3rd roll, not frame score") {
+                        let score: Int = (4 + 6) + (6*2 + 4)
+                        expect(game.score) == score
                     }
                     
-                    context("when next 2 roll also spare") {
+                    context("when roll 2") {
                         beforeEach {
-                            game.spare(first: 4)
+                            game.roll(2)
                         }
                         
-                        it("should double 3rd roll, not frame score") {
-                            let score: Int = (2 + 8) + (4*2 + 6)
+                        it("should double") {
+                            let score: Int = (4 + 6) + (6*2 + 4) + 2*2
                             expect(game.score) == score
                         }
-                        
-                        context("when roll 2") {
-                            beforeEach {
-                                game.roll(2)
-                            }
-                            
-                            it("should double") {
-                                let score: Int = (2 + 8) + (4*2 + 6) + 2*2
-                                expect(game.score) == score
-                            }
-                        }
+                    }
+                }
+                
+                context("when roll same spare") {
+                    beforeEach {
+                        game.spare(first: 2)
                     }
                     
-                    context("when roll same spare") {
-                        beforeEach {
-                            game.spare(first: 2)
-                        }
-                        
-                        it("should not find spare between 8 and 2") {
-                            let score: Int = (2 + 8) + (2*2 + 8)
-                            expect(game.score) == score
-                        }
+                    it("should not find spare between 8 and 2") {
+                        let score: Int = (2 + 8) + (2*2 + 8)
+                        expect(game.score) == score
                     }
                 }
             }
             
             // MARK: - Strike
-            describe("strike") {
-                context("when roll strike") {
+            context("when roll strike") {
+                beforeEach {
+                    game.strike()
+                }
+                
+                it("should double two next roll") {
+                    game.roll(4)
+                    game.roll(2)
+                    
+                    expect(game.score) == 10 + 2*4 + 2*2
+                }
+                
+                context("when two next roll also stike") {
                     beforeEach {
+                        game.strike()
                         game.strike()
                     }
                     
-                    it("should double two next roll") {
-                        game.roll(4)
-                        game.roll(2)
-                        
-                        expect(game.score) == 10 + 2*4 + 2*2
-                    }
-                    
-                    context("when two next roll also stike") {
-                        beforeEach {
-                            game.strike()
-                            game.strike()
-                        }
-                        
-                        it("should double both, and 2nd double 3rd") {
-                            expect(game.score) == 10 + 2*10 + 3*10
-                        }
+                    it("should double both, and 2nd double 3rd") {
+                        expect(game.score) == 10 + 2*10 + 3*10
                     }
                 }
             }
-            
+
             // MARK: - Endgame
             describe("ending") {
                 it("on start game not finished") {
                     expect(game.isFinished) == false
                 }
-                
+
                 context("when game is played 19 times") {
                     beforeEach {
                         game.roll(1, times: 19)
                     }
-                    
+
                     it("should not finish game") {
                         expect(game.isFinished) == false
                     }
                 }
-                
+
                 context("when game is played 20 times") {
                     beforeEach {
                         game.roll(1, times: 20)
                     }
-                    
+
                     it("should finish game") {
                         expect(game.isFinished) == true
                     }
-                    
+
                     context("when roll after game end") {
                         beforeEach {
                             game.roll(1)
                         }
-                        
+
                         it("should not add pins to score") {
                             expect(game.score) == 20
                         }
                     }
                 }
-                
+
                 describe("last frame") {
                     beforeEach {
                         game.roll(1, times: 18)
                     }
-                    
+
                     context("when spare") {
                         beforeEach {
                             game.spare()
                         }
-                        
+
                         it("should allow additioanal roll") {
                             expect(game.isFinished) == false
                         }
-                        
+
                         context("when perform additional roll") {
                             beforeEach {
                                 game.roll(3)
@@ -172,22 +167,22 @@ class GameSpec: QuickSpec {
                             it("should finish game") {
                                 expect(game.isFinished) == true
                             }
-                            
+
                             it("should double last roll") {
                                 expect(game.score) == 18 + 10 + 3*2
                             }
                         }
                     }
-                    
+
                     context("when stike") {
                         beforeEach {
                             game.strike()
                         }
-                        
+
                         it("should allow two additional roll") {
                             expect(game.isFinished) == false
                         }
-                        
+
                         context("when perform 1st additional roll") {
                             beforeEach {
                                 game.roll(3)
@@ -195,7 +190,7 @@ class GameSpec: QuickSpec {
                             it("should finish game") {
                                 expect(game.isFinished) == false
                             }
-                            
+
                             context("when perform 2nd additional roll") {
                                 beforeEach {
                                     game.roll(3)
@@ -208,12 +203,12 @@ class GameSpec: QuickSpec {
                     }
                 }
             }
-            
+
             context("when roll 10 stike in row") {
                 beforeEach {
                     game.roll(10, times: 10)
                 }
-                
+
                 describe("should allow two additional roll") {
                     context("when perform 1st stike") {
                         beforeEach {
@@ -222,7 +217,7 @@ class GameSpec: QuickSpec {
                         it("should not finish game") {
                             expect(game.isFinished) == false
                         }
-                        
+
                         context("when perform 2nd stike") {
                             beforeEach {
                                 game.strike()
@@ -230,7 +225,7 @@ class GameSpec: QuickSpec {
                             it("should finish game") {
                                 expect(game.isFinished) == true
                             }
-                            
+
                             it("should hit maximum score") {
                                 expect(game.score) == 300
                             }
